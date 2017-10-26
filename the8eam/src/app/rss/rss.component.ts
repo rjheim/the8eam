5da0ba25080f89c36864556a31e882e51026c4cb
rjheim <snoopyrj7@gmail.com>
2017-10-26 16:06:38 -0500

    Added DAL capabilities.

--- the8eam/src/app/rss/rss.component.ts
+++ the8eam/src/app/rss/rss.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { DataAccessLayerComponent } from '../data-access-layer/data-access-layer.component';
+import { AngularFirestoreCollection } from 'angularfire2/firestore';
+import { Event } from '../data-access-layer/event';
+//import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-rss',
+  providers: [DataAccessLayerComponent],
   templateUrl: './rss.component.html',
   styleUrls: ['./rss.component.css']
 })
 export class RssComponent implements OnInit {
+  eventToAdd: Event;
+  calendar: AngularFirestoreCollection<Event>;
+  list: AngularFirestoreCollection<Event>;
 
-  constructor() { }
+  constructor(private dal: DataAccessLayerComponent) {
+    this.eventToAdd = {} as Event;
+  }
 
   ngOnInit() {
+
   }
 
 }
