c8ab7c9d0ba9b067f5bd46319bfd9dc03a5257eb
rjheim <snoopyrj7@gmail.com>
2017-10-30 17:06:22 -0500

    Fixed DAL to represent new data management system

--- the8eam/src/app/data-access-layer/data-access-layer.component.ts
+++ the8eam/src/app/data-access-layer/data-access-layer.component.ts
@@ -12,23 +12,12 @@ import { Event } from './event';
 
 @Injectable ()
 export class DataAccessLayerComponent {
-  calendar: AngularFirestoreCollection<Event>;
   list: AngularFirestoreCollection<Event>;
-  calendarItems: Observable<Event[]>;
   listItems: Observable<Event[]>;
   model: Event;
-  model2: Event;
 
   constructor(db: AngularFirestore) {
-  	this.calendar = db.collection<Event>('calendar');
   	this.list = db.collection<Event>('list');
-  	this.calendarItems = this.calendar.snapshotChanges().map(actions => {
-  	  return actions.map(a => {
-        const data = a.payload.doc.data() as Event;
-        const id = a.payload.doc.id;
-        return { id, ...data };
-      });
-    });
   	this.listItems = this.list.snapshotChanges().map(actions => {
       return actions.map(a => {
         const data = a.payload.doc.data() as Event;
@@ -37,32 +26,16 @@ export class DataAccessLayerComponent {
       });
     });
     this.model = {} as Event;
-    this.model2 = {} as Event;
-    this.model2.report = 0;
     this.model.report = 0;
-  }
-
-  getCalendar () {
-    return this.calendarItems;
   }
 
   getList() {
     return this.listItems;
   }
 
-  addToCalendar(event: Event)
-  {
-    this.calendar.add(event);
-  }
-
   addToList(event: Event)
   {
   	this.list.add(event);
-  }
-
-  removeFromCalendar(key: string)
-  {
-  	this.calendar.doc(key).delete();
   }
 
   removeFromList(key: string)
@@ -71,10 +44,6 @@ export class DataAccessLayerComponent {
   }
 
   practiceAdd(){
-    this.addToCalendar(this.model);
-  }
-
-  practiceAdd2 (){
-    this.addToList(this.model2);
+    this.addToList(this.model);
   }
 }
