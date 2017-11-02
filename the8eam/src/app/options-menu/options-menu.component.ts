071a003d640796753a2f9182b2f62fccf3252f51
Nicole Backus <nicolebackus@Nicoles-MacBook-Pro.local>
2017-11-01 22:52:51 -0500

    Changed options menu, not using this for iteration2

--- the8eam/src/app/options-menu/options-menu.component.ts
+++ the8eam/src/app/options-menu/options-menu.component.ts
@@ -6,12 +6,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./options-menu.component.css']
 })
 export class OptionsMenuComponent implements OnInit {
-  genreTab = true;
-  priceTab = false;
-  locationTab = false;
-  dateTab = false;
-  constructor() { }
 
+  genreTab: boolean;
+  priceTab: boolean;
+  locationTab: boolean;
+  dateTab: boolean;
+
+  constructor() {
+    this.genreTab = true;
+    this.priceTab = false;
+    this.locationTab = false;
+    this.dateTab = false;
+  }
+  clickView(){
+    this.genreTab = true;
+    this.priceTab = false;
+    this.locationTab = false;
+    this.dateTab = false;
+  }
+  clickView2(){
+    this.genreTab = false;
+    this.priceTab = false;
+    this.locationTab = false;
+    this.dateTab = true;
+  }
+  clickView3(){
+    this.genreTab = false;
+    this.priceTab = true;
+    this.locationTab = false;
+    this.dateTab = false;
+  }
+  clickView4(){
+    this.genreTab = false;
+    this.priceTab = false;
+    this.locationTab = true;
+    this.dateTab = false;
+  }
   ngOnInit() {
   }
 
