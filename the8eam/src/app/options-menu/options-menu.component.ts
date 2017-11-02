3559294214c78247c734d744db814c8b515859dd
jviall <jamesviall@gmail.com>
2017-11-01 22:46:25 -0500

    Added tabbing functionality to options menu.

--- the8eam/src/app/options-menu/options-menu.component.ts
+++ the8eam/src/app/options-menu/options-menu.component.ts
@@ -6,12 +6,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./options-menu.component.css']
 })
 export class OptionsMenuComponent implements OnInit {
-  genreTab = true;
-  priceTab = false;
-  locationTab = false;
-  dateTab = false;
+  genreTabbed = true;
+  priceTabbed = false;
+  locationTabbed = false;
+  dateTabbed = false;
   constructor() { }
-
+  genreTab(){
+    this.genreTabbed = true;
+    this.priceTabbed = !this.genreTabbed;
+    this.locationTabbed = !this.genreTabbed;
+    this.dateTabbed = !this.genreTabbed;
+  }
+  priceTab(){
+    this.priceTabbed = true;
+    this.genreTabbed = !this.priceTabbed;
+    this.locationTabbed = !this.priceTabbed;
+    this.dateTabbed = !this.priceTabbed;
+  }
+  locationTab(){
+    this.locationTabbed = true;
+    this.priceTabbed = !this.locationTabbed;
+    this.genreTabbed = !this.locationTabbed;
+    this.dateTabbed = !this.locationTabbed;
+  }
+  dateTab(){
+    this.dateTabbed = true;
+    this.priceTabbed = !this.dateTabbed;
+    this.locationTabbed = !this.dateTabbed;
+    this.genreTabbed = !this.dateTabbed;
+  }
   ngOnInit() {
   }
 
