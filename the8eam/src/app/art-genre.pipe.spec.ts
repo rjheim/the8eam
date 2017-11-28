0cea0b5de06ee79d9e79ec39655ed4801d74646d
rjheim <snoopyrj7@gmail.com>
2017-11-27 21:49:21 -0600

    App code-coverage ~100%

--- the8eam/src/app/art-genre.pipe.spec.ts
+++ the8eam/src/app/art-genre.pipe.spec.ts
@@ -1,8 +1,33 @@
 import { ArtGenrePipe } from './art-genre.pipe';
+import { Event } from './event';
 
 describe('ArtGenrePipe', () => {
-  it('should create an instance', () => {
-    const pipe = new ArtGenrePipe();
+  const pipe = new ArtGenrePipe();
+  let testEvent1 = new Event();
+  let testEvent2 = new Event();
+  let inputList: Event[] = [];
+  let outputList: Event[] = [];
+
+  it('create an instance', () => {
     expect(pipe).toBeTruthy();
+  });
+
+  it ('Filters to only show art genre items', () => {
+    testEvent1.genre = "art";
+    testEvent2.genre = "dance";
+    inputList.push(testEvent2);
+    inputList.push(testEvent1);
+    outputList.push(testEvent1);
+    expect(pipe.transform(inputList, true)[0].date).toBe(outputList[0].date);
+  });
+
+  it ('False so don\'t filter', () => {
+    testEvent1.genre = "art";
+    testEvent2.genre = "dance";
+    inputList.push(testEvent2);
+    inputList.push(testEvent1);
+    outputList.push(testEvent2);
+    outputList.push(testEvent1);
+    expect(pipe.transform(inputList, false)[0].date).toBe(outputList[0].date);
   });
 });
--- the8eam/src/app/filter-vars.service.spec.ts
+++ the8eam/src/app/filter-vars.service.spec.ts
@@ -3,13 +3,56 @@ import { TestBed, inject } from '@angular/core/testing';
 import { FilterVarsService } from './filter-vars.service';
 
 describe('FilterVarsService', () => {
+  let service: FilterVarsService;
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [FilterVarsService]
-    });
+     service = new FilterVarsService();
   });
 
-  it('should be created', inject([FilterVarsService], (service: FilterVarsService) => {
+  it('should be created', () => {
     expect(service).toBeTruthy();
-  }));
+  });
+
+  let cost: number;
+  let date: number;
+
+  it('should set music to false if first true (and vice versa)', () => {
+    service.setMusic();
+    expect(service.gMusic).toBeTruthy();
+  });
+  it('should set art to false if first true (and vice versa)', () => {
+    service.setArt();
+    expect(service.gArt).toBeTruthy();
+  });
+  it('should set food to false if first true (and vice versa)', () => {
+    service.setFood();
+    expect(service.gFood).toBeTruthy();
+  });
+  it('should set SW to false if first true (and vice versa)', () => {
+    service.setSW();
+    expect(service.gSW).toBeTruthy();
+  });
+  it('should set dance to false if first true (and vice versa)', () => {
+    service.setDance();
+    expect(service.gDance).toBeTruthy();
+  });
+  it('Should set cost', () => {
+    cost = 10;
+    service.setCost(cost);
+    expect(service.cost).toBe(10);
+  });
+  it('Should set cost to negative one to uncheck (set/clicked twice)', () => {
+    service.setCost(cost);
+    service.setCost(cost);
+    expect(service.cost).toBe(-1);
+  });
+  it('Should set cost', () => {
+    date = 20171127;
+    service.setDate(date);
+    expect(service.date).toBe(20171127);
+  });
+  it('Should set cost to negative one to uncheck (set/clicked twice)', () => {
+    service.setDate(date);
+    service.setDate(date);
+    expect(service.date).toBe(-1);
+  });
 });
