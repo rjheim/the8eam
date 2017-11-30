6f2ce32d64ce5c9be04d9c6a9afa854e32ac95d7
notjameson <jzaballos@wisc.edu>
2017-11-29 18:58:42 -0600

    improved tagging algorithm

--- the8eam/src/app/rss.service.ts
+++ the8eam/src/app/rss.service.ts
@@ -13,20 +13,29 @@ import * as converter from 'ical2json';
 
 // rssGet = require('rss-to-json');
 // ALL OF THE SETS WE WILL BE COMPARING DESCRIPTIONS TO
-var musicArray = ["MUSIC", "SONG", "SINGING", "JAZZ", "ROCK", "PIANO", "GUITAR", "DRUMS", "CONCERT", "SING-ALONG", "OPEN MIC", "SOUL", "AMERICANA"];
+var musicArray = ["MUSIC", "SONG", "SINGING", "JAZZ", "ROCK", "PIANO", "GUITAR", "DRUMS", "CONCERT", "SING-ALONG", "MIC", "SOUL", "AMERICANA", "RAP", "RAPPER", "SINGER", "POP", "BLUES", "FOLK", "HIP", "COUNTRY", "ELECTRONIC", "RAVE", "REGGAE", "CLASSICAL", "PUNK", "FUNK", "RHYTHM", "TECHNO", "DISCO", "METAL", "DUBSTEP", "OPERA"];
 let musicSet: Set<string> = new Set<string>(musicArray);
 
-var spokenWordArray = ["SPOKEN", "WORD", "SLAM", "POETRY", "POET"];
-let spokenWordSet: Set<string> = new Set<string>(spokenWordArray);
+var literaryArray = ["WORD", "SLAM", "POETRY", "POET", "LITERATURE", "BOOK", "BOOKS", "WRITE", "WRITING", "NOVEL", "WRITER", "AUTHOR", "READING", "SIGNING", "READ"];
+let literarySet: Set<string> = new Set<string>(literaryArray);
 
 var artArray = ["ARCHITECTURE", "CERAMICS", "COLLAGE", "DESIGN", "DRAWING", " CHALK", "CHARCOAL", "PASTEL", "GRAFFITI", "GRAPHIC", "GRAPHICS", "ILLUSTRATION", "JEWELLERY", "METALWORK", "MOSAIC", "PAINT", "PAINTING", "PAINTER", "ART", "ARTIST", "ACRYLICS", "OILS", "OIL", "PHOTO", "PHOTOGRAPHY", "PRINT", "ENGRAVING", "SCULPTURE", "STAINED GLASS", "TAPESTRY", "ABSTRACT", "PORTRAIT", "LANDSCAPE", "IMPRESSIONISM", "POST-IMPRESSIONISM", "CUBISM", "SURREALISM"];
 let artSet: Set<string> = new Set<string>(artArray);
 
-var danceArray = ["WALTZ", "TANGO", "CHACHA", "CHA-CHA", "RUMBA", "SAMBA", "MAMBO", "QUICKSTEP", "JIVE", "BOLERO", "CHARLESTON", "SWING", "TAP", "BOOGIE", "SALSA", "FLAMENCO", "LAMBADA", "POLKA", "JIVE", "CAPOERIA", "LINE DACE", "BELLY DANCE", "FOLK DANCE", "BALLET", "CONTEMPORARY DANCE", "MODERN DANCE", "DISCO", "BOLLYWOOD", "BREAKDANCE", "BALLROOM", "IRISH", "HUSTLE", "JITTERBUG", "FOXTROT", "MERENGUE", "ZUMBA", "POLE DANCE", "DANCE"];
+var danceArray = ["WALTZ", "TANGO", "CHACHA", "CHA-CHA", "RUMBA", "SAMBA", "MAMBO", "QUICKSTEP", "JIVE", "BOLERO", "CHARLESTON", "SWING", "TAP", "BOOGIE", "SALSA", "FLAMENCO", "LAMBADA", "POLKA", "JIVE", "CAPOERIA", "LINE DACE", "BELLY DANCE", "FOLK DANCE", "BALLET", "CONTEMPORARY", "MODERN", "DISCO", "BOLLYWOOD", "BREAKDANCE", "BALLROOM", "IRISH", "HUSTLE", "JITTERBUG", "FOXTROT", "MERENGUE", "ZUMBA", "POLE DANCE", "DANCE"];
 let danceSet: Set<string> = new Set<string>(danceArray);
 
-var foodArray = ["MARKET", "BREAKFAST", "BRUNCH", "LUNCH", "TEA", "DINNER", "SUPPER", "SNACK", "MEAL", "DESSERT", "HORS D’OEUVRE", "ENTREE", "SIDE", "BANQUET", "BUFFET", "CUISINE", "DRINK", "EAT", "EATING", "VEGAN", "VEGETARIAN", "ITALIAN", "SEAFOOD", "MEXICAN", "VIETNAMESE", "THAI", "SAUSAGE", "BARBECUE", "BARBEQUE", "BBQ", "SUSHI", "HOT DOG", "BURGER", "STEAK", "MEDITERRANEAN", "PERUVIAN", "GREEK", "BRAZILIAN", "ASIAN", "SOUL FOOD", "DONUT", "DOUGHNUT", "BEER", "ICE CREAM", "KOREAN", "FOOD TRUCK", "TAPAS", "STREET FOOD", "TACO", "TASTE", "SIP", "MUNCH", "RESTAURANT", "BAR", "DINER"];
+var foodArray = ["MARKET", "BREAKFAST", "BRUNCH", "LUNCH", "TEA", "DINNER", "SUPPER", "SNACK", "MEAL", "DESSERT", "HORS", "D’OEUVRE", "ENTREE", "SIDE", "BANQUET", "BUFFET", "CUISINE", "DRINK", "EAT", "EATING", "VEGAN", "VEGETARIAN", "ITALIAN", "SEAFOOD", "MEXICAN", "VIETNAMESE", "THAI", "SAUSAGE", "BARBECUE", "BARBEQUE", "BBQ", "SUSHI", "HOT DOG", "BURGER", "STEAK", "MEDITERRANEAN", "PERUVIAN", "GREEK", "BRAZILIAN", "ASIAN", "SOUL FOOD", "DONUT", "DOUGHNUT", "BEER", "ICE", "KOREAN", "FOOD TRUCK", "TAPAS", "TACO", "TASTE", "SIP", "MUNCH", "RESTAURANT", "BAR", "DINER"];
 let foodSet: Set<string> = new Set<string>(foodArray);
+
+var familyArray = ["DAYCARE","KID", "KIDS", "CHILD", "PARENTAL","PARENT", "MOM","DAD", "CHILDREN","LEGO", "PARENTS","MOTHER","FATHER", "CARE", "KINDERGARTEN","ELEMENTARY", "SCHOOL", "MIDDLE","TOY", "PLAYGROUND", "FAMILY", "FAMILIES"]
+let familySet: Set<string> = new Set<string>(familyArray);
+
+var theaterArray = ["THEATER", "STAGE", "DRAMA", "MUSICAL", "SHAKESPEARE", "THEATRE", "THEATRICAL", "ACT", "ACTING", "ACTRESS", "COMEDY", "TRAGEDY", "DRAMA", "ACTORS", "OPERA", "PLAY"]
+let theaterSet: Set<string> = new Set<string>(theaterArray);
+
+var filmArray = ["MOVIE", "FILM", "ACTRESS","FILMING","THEATER", "ACTOR","ACTORS","ACTRESSES", "ACTION","ADVENTURE","COMEDY","THRILLER", "DRAMA","HOLLYWOOD","BOLLYWOOD","DIRECTOR","TRAILER", "WUD","UNION", "CINEMA","CARTOON","ANIME","SOUNDTRACK","COSTUME","DOCUDRAMA","DOCUMENTARY","FILMSTRIP"]
+let filmSet: Set<string> = new Set<string>(filmArray);
 
 
 @Injectable()
@@ -188,13 +197,40 @@ export class RssService {
   categorizer(description, eventName) {
 
 
-    let music: boolean = false;
-    let spokenWord: boolean = false;
-    let dance: boolean = false;
-    let food: boolean = false;
-    let art: boolean = false;
+    let music: number = 0;
+    let literary: number = 0;
+    let dance: number = 0;
+    let food: number = 0;
+    let art: number = 0;
+    let family: number = 0;
+    let film: number = 0;
+    let theater: number = 0;
 
+    // trim the punctuation
+
+
+
+
+    var descr = description.split(" ");
+
+    for (var word in descr) {
+      //remove beginning and end punctuation
+      if(word.endsWith('.') || word.endsWith('?') || word.endsWith('!') || word.endsWith(',') || word.endsWith(';') ||
+      word.endsWith(':') || word.endsWith('\'') || word.endsWith('\"')) {
+        word = word.substr(0, word.length - 1);
+      }
+
+      if(word.startsWith('\"') || word.startsWith('\'')) {
+        word = word.substr(1);
+      }
+
+      // make the word capitalized to match our sets
+      word = word.toUpperCase();
+
+
+    }
     // CHECK MUSIC SET
+    /*
     musicSet.forEach(function (item) {
       if (description.indexOf(item) != -1) {
         music = true;
@@ -204,8 +240,15 @@ export class RssService {
         music = true;
       }
     }, this);
+    */
+
+    if(musicSet.has(word))
+    {
+      ++music;
+    }
 
     // CHECK SPOKEN WORD SET
+    /*
     spokenWordSet.forEach(function (item) {
       if (description.indexOf(item) != -1) {
         spokenWord = true;
@@ -214,8 +257,14 @@ export class RssService {
         spokenWord = true;
       }
     }, this);
+    */
+    if(literarySet.has(word))
+    {
+      ++literary;
+    }
 
     // CHECK FOOD SET
+    /*
     foodSet.forEach(function (item) {
       if (description.indexOf(item) != -1) {
         food = true;
@@ -224,8 +273,15 @@ export class RssService {
         food = true;
       }
     }, this);
+    */
+
+    if(foodSet.has(word))
+    {
+      ++food;
+    }
 
     // CHECK ART SET
+    /*
     artSet.forEach(function (item) {
       if (description.indexOf(item) != -1) {
         art = true;
@@ -234,8 +290,16 @@ export class RssService {
         art = true;
       }
     }, this);
+  */
+
+    if(artSet.has(word))
+    {
+      ++art;
+    }
 
     // CHECK DANCE SET
+
+    /*
     danceSet.forEach(function (item) {
       if (description.indexOf(item) != -1) {
         dance = true;
@@ -244,35 +308,67 @@ export class RssService {
         dance = true;
       }
     }, this);
+  */
 
+    if(danceSet.has(word))
+    {
+      ++dance;
+    }
+
+    // CHECK FAMILY SET
+    if(familySet.has(word)){
+      ++family;
+    }
+
+    // CHECK THEATER SET
+    if(theaterSet.has(word)){
+      ++theater;
+    }
+
+    // CHECK FILM SET
+    if(filmSet.has(word)){
+      ++film;
+    }
 
     var genres = "";
 
-    if (music) {
+    if (music > 0) {
       genres = genres + "music;";
       //eventToAdd.genre.push("music");
     }
 
-    if (spokenWord) {
-      genres = genres + "spoken word;";
+    if (literary > 0) {
+      genres = genres + "literature;";
       //eventToAdd.genre.push("spoken word");
     }
 
-    if (dance) {
+    if (dance > 0) {
       genres = genres + "dance;";
       //eventToAdd.genre.push("dance");
     }
 
-    if (art) {
+    if (art > 0) {
       genres = genres + "art;";
       //eventToAdd.genre.push("art");
     }
 
-    if (food) {
+    if (food > 0) {
       genres = genres + "food;";
       //eventToAdd.genre.push("food");
     }
-    //console.log(eventToAdd.genre);
+
+    if (family > 0) {
+      genres = genres + "family;"
+    }
+
+    if (theater > 0) {
+      genres = genres + "theater;"
+    }
+
+    if (film > 0) {
+      genres = genres + "film;"
+    }
+
     return genres;
   }
 
@@ -286,7 +382,10 @@ export class RssService {
 
 
         for (var key in items) {
+          console.log(key);
+
           var eventToAdd = {} as Event;
+          let toAdd: boolean = true;
 
           var title = items[key]["title"];
           var titleIndex = title.indexOf(" - ");
@@ -337,14 +436,20 @@ export class RssService {
           eventName = eventName.toUpperCase();
 
           eventToAdd.genre = this.categorizer(description, eventName);
+          console.log(eventToAdd.genre);
+
+          if(eventToAdd.genre == "" || eventToAdd.genre == "family;"){
+            toAdd = false;
+          }
 
 
           // set default report value to ZERO
           eventToAdd.report = 0;
 
-
-          if (list.indexOf(eventToAdd) == -1) {
-            list.push(eventToAdd);
+          if(toAdd) {
+            if (list.indexOf(eventToAdd) == -1) {
+              list.push(eventToAdd);
+            }
           }
 
           //console.log(list);
