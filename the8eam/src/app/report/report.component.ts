d3f80d8def1c37bbef382b77c2cceb295b9110cb
rjheim <snoopyrj7@gmail.com>
2017-12-05 23:30:47 -0600

    MODAL FINALLY DOESN"T HAVE THE BACKGROUD SCROLLL THANK THE LOOORDDDD

--- the8eam/src/app/report/report.component.ts
+++ the8eam/src/app/report/report.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import {Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
 import { Event } from '../event';
 import { Report } from '../report';
 import {DataAccessLayerService} from '../data-access-layer.service';
@@ -9,14 +9,19 @@ import {DataAccessLayerService} from '../data-access-layer.service';
   templateUrl: './report.component.html',
   styleUrls: ['./report.component.css']
 })
-export class ReportComponent {
+export class ReportComponent implements OnInit {
+  @Output()
+  reporting = new EventEmitter<boolean>();
   @Input()
   eventItem: Event;
   reportAdd: Report;
+  showAlert1: boolean;
+  showAlert2: boolean;
+  showAlert3: boolean;
+  reportSelected: string;
   email: string;
   description: string;
-  reasNum: number;
-  reason: string[] = ['Inaccurate Information',
+  reason: string[] = ['\<Select Reason\>','Inaccurate Information',
     'Inappropriate', 'Duplicate Item',
     'This isn\'t an Art Event'];
 
@@ -26,24 +31,43 @@ export class ReportComponent {
   constructor(public dal: DataAccessLayerService) {
     this.eventItem = {} as Event;
     this.reportAdd = {} as Report;
-    this.reasNum = 0;
+    this.showAlert1 = false;
+    this.showAlert2 = false;
+    this.showAlert3 = false;
     this.email = '';
     this.description = '';
   }
+  ngOnInit(){
+    this.show();
+  }
 
+  updateReason(s: string){
+    this.reportSelected = s;
+  }
 
-  submitFeedback(id: string): void {
+  submitFeedback(): void {
     //to implement
+    const pureEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     this.eventItem.report++;
     this.reportAdd.id = this.eventItem.id;
     this.reportAdd.description = this.description;
+    if (!this.description){
+      this.showAlert2 = true;
+    }
     this.reportAdd.email = this.email;
-    // reason doesn't work rn
-    this.reportAdd.reason = this.reason[this.reasNum];
-    console.log(this.reportAdd);
-    this.dal.updateDoc(id, this.eventItem);
-    this.dal.addToReport(this.reportAdd);
-    this.hide();
+    if (pureEmail.test(this.email) && this.email){
+     this.showAlert3 = true;
+    }
+    this.reportAdd.reason = this.reportSelected;
+    if (this.reportSelected == this.reason[0]){
+      this.showAlert1 = true;
+    }
+    if (!this.showAlert3 && !this.showAlert2 && !this.showAlert1) {
+      console.log(this.reportAdd);
+      //this.dal.updateDoc(this.eventItem.id, this.eventItem);
+      //this.dal.addToReport(this.reportAdd);
+      this.hide();
+    }
   }
 
   /*
@@ -52,19 +76,13 @@ export class ReportComponent {
   show(): void {
     this.visible = true;
     setTimeout(() => this.visibleAnimate = true, 100);
-    // jQuery("#reportModal").on(this.show(), function() {
-    //   jQuery("body").addClass("no-scroll-on-modal");
-    //   jQuery("html").addClass("no-scroll-on-modal");
-    // });
   }
 
   hide(): void {
+    this.showAlert1 = false; this.showAlert2 = false; this.showAlert3 = false;
     this.visibleAnimate = false;
+    this.reporting.emit(false);
     setTimeout(() => this.visible = false, 300);
-    // jQuery("#reportModal").on(this.show(), function() {
-    //   jQuery("body").removeClass("no-scroll-on-modal");
-    //   jQuery("html").removeClass("no-scroll-on-modal");
-    // });
   }
 
   onContainerClicked(event: MouseEvent): void {
