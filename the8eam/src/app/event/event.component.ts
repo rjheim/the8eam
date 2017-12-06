3c4574ff94fe51e3643457db583d8fe3bc910a27
jviall <jamesviall@gmail.com>
2017-12-06 14:14:16 -0600

    made genres display better in event items

--- the8eam/src/app/event/event.component.ts
+++ the8eam/src/app/event/event.component.ts
@@ -9,14 +9,21 @@ import { Event } from '../event';
 export class EventComponent implements OnInit {
   @Input()
   eventItem: Event;
+  genres;
   showInfo : boolean;
   report: boolean;
   constructor() {
     this.showInfo = false;
     this.report = false;
+    this.genres = [];
   }
 
   ngOnInit() {
+    this.genres = this.eventItem.genre.split(';').map(function(str){
+      return str.charAt(0).toUpperCase() + str.slice(1);
+    });
+    this.genres = this.genres.slice(0, this.genres.length - 1); // remove tail element cause it's always empty
+    console.log(this.genres);
   }
 
   toggleInfo(){
